c41d19d40795448a16b91bbb3a01e34d30aa4379
Елисеев Александр Александрович <a.eliseev@psuti.ru>
2024-03-06 20:17:30 +0400

    test

--- turtleController.ino
+++ turtleController.ino
@@ -1,37 +1,26 @@
-#include <Wire.h>
-#include <I2C_RTC.h> // https://github.com/cvmanjoo/RTC
+#include "RTClib.h"
+
+RTC_DS3231 rtc;
 
 #define UVrelay 4
 #define IKrelay 5 // Normally closed relay, invert HIGH and LOW
 
-// Set date, time, week
-#define date 17,12,22
-#define time 13,19,00
-#define week 7 // 1 - Sunday, 2 - Monday, etc.
 // Set uv and ik lamps working period
-#define uvOnHour 8
-#define uvOffHour 14
-#define ikOnHour 8
-#define ikOffHour 23
-
-bool isSetTime = false; // True if want set up time
-static PCF8563 RTC;
-
-void taskSetTime() {
-  RTC.setDate(date);
-  RTC.setTime(time);
-  RTC.setWeek(week);
-}
+#define uvOnHour 7
+#define uvOffHour 20
+#define ikOnHour 7
+#define ikOffHour 20
 
 void taskSetRelay() {
-  if (RTC.getHours() >= uvOnHour-1 && RTC.getHours() <= uvOffHour-1) {
+  DateTime now = rtc.now();
+  if (now.hour() >= uvOnHour && now.hour() <= uvOffHour) {
     digitalWrite(UVrelay, LOW);
   }
   else {
     digitalWrite(UVrelay, HIGH);
   }
 
-  if (RTC.getHours() >= ikOnHour-1 && RTC.getHours() <= ikOffHour-1) { // Normally closed relay
+  if (now.hour() >= ikOnHour && now.hour() <= ikOffHour) { // Normally closed relay
     digitalWrite(IKrelay, HIGH);
   }
   else {
@@ -40,16 +29,18 @@ void taskSetRelay() {
 }
 
 void setup() {
-  RTC.begin();
-
+  Serial.begin(9600);
   pinMode(UVrelay, OUTPUT);
   pinMode(IKrelay, OUTPUT);
-
-  if (isSetTime) {
-    taskSetTime();
-  }
 }
 
 void loop() {
-  taskSetRelay();
+  //taskSetRelay();
+  DateTime now = rtc.now();
+  Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
 }
